27ca37fad59849abd0737e2518815709fefbea61
jw.kim <babamba88@gintlab.com>
2020-11-24 16:59:30 +0900

    feat: :recycle: recap

--- src/jwt/jwt.middleware.ts
+++ src/jwt/jwt.middleware.ts
@@ -18,16 +18,17 @@ export class JwtMiddleWare implements NestMiddleware {
   async use(req: Request, res: Response, next: NextFunction) {
     if ('x-jwt' in req.headers) {
       const token = req.headers['x-jwt'];
-      const decoded = this.jwtService.verify(token.toString());
-      if (typeof decoded === 'object' && decoded.hasOwnProperty('id')) {
-        console.log(decoded['id']);
-        try {
+
+      try {
+        const decoded = this.jwtService.verify(token.toString());
+        if (typeof decoded === 'object' && decoded.hasOwnProperty('id')) {
+          console.log(decoded['id']);
           const user = await this.userService.findById(decoded['id']);
           req['user'] = user;
-          // console.log('user : ', user);
-        } catch (error) {
-          console.log('error');
         }
+        // console.log('user : ', user);
+      } catch (error) {
+        console.log('error');
       }
     }
     next();
